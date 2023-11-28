98b9a4fefd30c0de8092cdc996bdc2bc1634fac9
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2023-11-28 10:00:58 +0300

    finished last test

--- homework.py
+++ homework.py
@@ -71,13 +71,12 @@ def test_find_suitable_user():
     ]
 
 
-# Сделайте функцию, которая будет печатать
-# читаемое имя переданной ей функции и значений аргументов.
+# Сделайте функцию, которая будет печатать читаемое имя переданной ей функции и значений аргументов.
 # Вызовите ее внутри функций, описанных ниже
 # Подсказка: Имя функции можно получить с помощью func.__name__
-# Например, вызов следующей функции должен преобразовать имя функции
-# в более читаемый вариант (заменить символ подчеркивания на пробел,
-# сделать буквы заглавными (или первую букву), затем вывести значения всех аргументов этой функции:
+# Например, вызов следующей функции должен преобразовать имя функции в более читаемый вариант
+# (заменить символ подчеркивания на пробел, сделать буквы заглавными (или первую букву),
+# затем вывести значения всех аргументов этой функции:
 # >>> open_browser(browser_name="Chrome")
 # "Open Browser [Chrome]"
 
@@ -85,19 +84,37 @@ def test_find_suitable_user():
 def test_readable_function():
     open_browser(browser_name="Chrome")
     go_to_companyname_homepage(page_url="https://companyname.com")
-    find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
+    find_registration_button_on_login_page(
+        page_url="https://companyname.com/login", button_text="Register"
+    )
+
+
+def make_pretty(function_name, *args):
+    function_name = function_name.split("_")
+    final_name = [word.title() for word in function_name]
+    # for word in function_name:
+    #     final_name.append(word.upper())
+    if len(args) > 1:
+        return f"{' '.join(final_name)} [{', '.join(args)}]"
+    else:
+        return f"{' '.join(final_name)} [{args[0]}]"
 
 
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = make_pretty(open_browser.__name__, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = make_pretty(go_to_companyname_homepage.__name__, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
-    assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+    actual_result = make_pretty(
+        find_registration_button_on_login_page.__name__, page_url, button_text
+    )
+    assert (
+        actual_result
+        == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+    )
